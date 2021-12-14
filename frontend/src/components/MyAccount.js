d79834ba18ee00104cfd84098cba3960f9e0517c
estefaniabet <estefaniabetancurvargas@gmail.com>
2021-12-14 10:35:38 -0500

    Se finaliza la creación del header

---
+++ frontend/src/components/MyAccount.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyAccount = () => {
+       return (
+              <div className="sec__one">
+                     <h1> Mi cuenta </h1>
+              </div>
+       )
+}
+
+export default MyAccount
