73948d82d1e72e39a5ee96ea866a353b5d3c8932
mafer23 <mafer18199420@gmail.com>
2021-12-15 11:47:35 -0500

    variables de entorno y conexion limpia

--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,18 +1,23 @@
 const boom = require('@hapi/boom');
-const getConnection = require('../libs/postgres');
+const pool = require('../libs/postgres.pool');
 
 
 class UserService {
 
-  constructor(){}
+  constructor(){
+    this.users = [];
+    this.pool = pool;
+    this.pool.on('error',(err) => console.error(eer));
+  }
 
   async create(data){
      return data;
   }
 
-  async find() {
-    const client = await getConnection();
-    const rta = await client.query('SELECT * FROM public.usuario');
+   async find() {
+
+    const query = 'SELECT * FROM public.usuario';
+    const rta = await this.pool.query(query);
     return rta.rows;
   }
      // const rta = await client.query('SELECT * FROM usuario ')
